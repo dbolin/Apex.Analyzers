dc322c2a32e1b736aeb4b10bf681bb16d572b2df
dbolin <dbolin.m@gmail.com>
2019-05-04 19:32:13 -0400

    Add test for nested generic

--- Immutable/Apex.Analyzers.Immutable.Test/ApexAnalyzersImmutableUnitTests.cs
+++ Immutable/Apex.Analyzers.Immutable.Test/ApexAnalyzersImmutableUnitTests.cs
@@ -309,6 +309,41 @@ namespace Apex.Analyzers.Immutable.Test
         }
 
         [TestMethod]
+        public void IMM003MemberFieldsNestedGenericNotImmutableConcrete()
+        {
+            var test = GetCode(@"
+    public class MutableClass
+    {
+    }
+
+    [Immutable]
+    public class Class1<T>
+    {
+        private readonly int x;
+        private readonly T Value;
+    }
+
+    [Immutable]
+    public class Test
+    {
+        private readonly Class1<Class1<MutableClass>> TestValue;
+    }
+");
+            var expected = new DiagnosticResult
+            {
+                Id = "IMM003",
+                Message = "Type of field 'TestValue' is not immutable because type argument 'MutableClass' is not immutable",
+                Severity = DiagnosticSeverity.Error,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 27, 55)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
         public void IMM003MemberFieldsGenericFromSystemNotImmutableConcrete()
         {
             var test = GetCode(@"
