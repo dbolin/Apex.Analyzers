d8db606aefa43f3993aff99d9e6bae647f7cdf70
dbolin <dbolin.m@gmail.com>
2019-05-10 18:55:56 -0400

    Fix author and add onFaith parameter to attribute

--- Immutable/Apex.Analyzers.Immutable.Attributes/ImmutableAttribute.cs
+++ Immutable/Apex.Analyzers.Immutable.Attributes/ImmutableAttribute.cs
@@ -3,5 +3,11 @@
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface, AllowMultiple = false, Inherited = false)]
     public sealed class ImmutableAttribute : Attribute
     {
+        public ImmutableAttribute(bool onFaith = false)
+        {
+            OnFaith = onFaith;
+        }
+
+        public bool OnFaith { get; }
     }
 }
