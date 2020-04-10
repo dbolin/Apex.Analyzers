d569e1e791a25b71b3e896bd18c031160cf38b57
Mark Welsh <mwelsh1118@gmail.com>
2020-04-10 14:23:10 -0500

    Switched to project reference from main analyzer project; deleted duplicate code

--- Immutable/Apex.Analyzers.Immutable/Rules/IMM004.cs
+++ Immutable/Apex.Analyzers.Immutable/Rules/IMM004.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using Apex.Analyzers.Immutable.Semantics;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace Apex.Analyzers.Immutable.Rules
